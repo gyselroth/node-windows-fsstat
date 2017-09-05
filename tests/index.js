8a702ac608056f34977ba1534185f6a05432674a
raffis <sahli@gyselroth.com>
2017-09-05 17:21:36 +0200

    fixes test

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,4 @@
 // note that the compiled addon is placed under following path
 var test = require('../build/Release/addon');
 // `Hello` function returns a string, so we have to console.log it!
-console.log(test.statSync('C:\\Flashtool'));
-console.log(test.statSync('C:\\Flashtool\\custom\\Drawing1.vsdx'));
+console.log(test.statSync('C:\\Windows'));
